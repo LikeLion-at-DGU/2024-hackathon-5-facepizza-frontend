71e86f94af9ee880a1b14ff4941c001133e8af81
jjjung0921 <earthenergy-1128@naver.com>
2024-07-24 14:44:05 +0900

    Feat: 메인화면 영상 연결

--- src/components/Face_Camera_Home.jsx
+++ src/components/Face_Camera_Home.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {useRef} from "react";
 import * as S from '../styles/StyledComponents';
 import photo from '../assets/photo.jpg'; // 사진 경로 수정 필요
 import album from '../assets/album.jpg'; // 사진 경로 수정 필요
 import calendar from '../assets/calrender.png';
 import bedge from '../assets/bedge.png';
+import VideoComponent from "./VideoComponent";
+import FaceDetection from "./FaceDetection";
 
 const Face_Camera_Home = () => {
+    const videoRef = useRef(null);
     return (
         <S.Face_Camera_Home>
             <S.Section>
                 <h2>내 표정 기록하기</h2>
                 <div id="cont_box">
                     <div id="left_box">
-                        <S.Image src={photo} alt="Selfie" id="camera_cover"/>
+                        <VideoComponent videoRef={videoRef} />
+                        <FaceDetection videoRef={videoRef}/>
                         <S.desc>
                             <h3>표정 사진 찍기</h3>
                             설명
--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -2,19 +2,30 @@ import React, { useEffect } from "react";
 
 const VideoComponent = ({ videoRef }) => {
   useEffect(() => {
+    if (!videoRef || !videoRef.current) {
+      console.log("videoRef가 전달되지 않았습니다.");
+      return;
+    }
+
     const startVideo = () => {
       navigator.mediaDevices
         .getUserMedia({ video: true })
         .then((stream) => {
           videoRef.current.srcObject = stream;
         })
-        .catch((err) => console.log("Error accessing video stream: ", err));
+        .catch((err) => console.log("비디오 스트림 접근 오류: ", err));
     };
 
     startVideo();
   }, [videoRef]);
 
-  return <video ref={videoRef} autoPlay={true} width="720" height="540" />;
+  return (
+    <video
+      ref={videoRef}
+      autoPlay={true}
+      style={{ width: "70%", borderRadius: "8px" }}
+    />
+  );
 };
 
 export default VideoComponent;
