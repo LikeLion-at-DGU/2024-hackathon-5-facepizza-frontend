878fc7f671a50a317b796d962e212ab9f9416c16
jjjung0921 <earthenergy-1128@naver.com>
2024-07-24 14:48:58 +0900

    Feat: main페이지 video 연결

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import VideoComponent from "./components/VideoComponent";
 import FaceDetection from "./components/FaceDetection";
 import LandmarkComponent from "./components/LandmarkComponent";
+import ExpressionComponent from "./components/ExpressionComponent";
 
 const MainComponent = () => {
   const videoRef = useRef();
@@ -11,7 +12,7 @@ const MainComponent = () => {
     <>
       <VideoComponent videoRef={videoRef} />
       <FaceDetection videoRef={videoRef} canvasRef={canvasRef} />
-      <LandmarkComponent/>
+      <ExpressionComponent/>
     </>
   );
 };
