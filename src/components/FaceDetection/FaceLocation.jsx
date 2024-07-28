7ffc43611f3087a9d5cdeaa0d30a12f676621b03
jjjung0921 <earthenergy-1128@naver.com>
2024-07-28 20:35:50 +0900

    FaceLocation 추가

--- src/components/FaceDetection/FaceLocation.jsx
+++ src/components/FaceDetection/FaceLocation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useEffect } from "react";
 import FaceDetection from "./FaceDetection";
 
 const FaceLocation = ({videoRef, onLandmarks}) => {
