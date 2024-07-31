3ea7b0f5e09a1d1eab9c8407de330a3a9b2f5b0d
iam-hyo <hyojunj@naver.com>
2024-08-01 00:25:08 +0900

    홈페이지 Ui 전환

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,13 @@ import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 // 컴포넌트 파일들 불러오기
 import Header from './components/Header';
 import AppRouter from './Router';
-import FacialStretch from './components/FacialStretch';
-import FacialExercise from './components/FacialExercise';
 import Home_Title from './components/Homepage/Home_Title';
 import Home_Content from './components/Homepage/Home_Content';
 // 스타일 컴포넌트 불러오기
 import { Main, Container } from './styles/StyledComponents';
 
 const AppContent = () => {
-  const [isStretchModalOpen, setIsStretchModalOpen] = useState(false);
-  const [isExerciseModalOpen, setIsExerciseModalOpen] = useState(false);
-
-  const openStretchModal = () => setIsStretchModalOpen(true);
-  const closeStretchModal = () => setIsStretchModalOpen(false);
-
-  const openExerciseModal = () => setIsExerciseModalOpen(true);
-  const closeExerciseModal = () => setIsExerciseModalOpen(false);
-
+  
   const location = useLocation();
   const isHome = location.pathname === '/';
 
@@ -30,8 +20,6 @@ const AppContent = () => {
       <Main>
         <AppRouter />
       </Main>
-      <FacialStretch isOpen={isStretchModalOpen} onRequestClose={closeStretchModal} />
-      <FacialExercise isOpen={isExerciseModalOpen} onRequestClose={closeExerciseModal} />
       {isHome && <Home_Content />}
     </>
   );
