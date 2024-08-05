61035fc775590447640d3745ab7139c69034edcd
jiyoon607 <jiyoon607@naver.com>
2024-08-06 08:20:39 +0900

    Fix:models경로설정

--- src/components/FaceDetection/LoadApiModels.jsx
+++ src/components/FaceDetection/LoadApiModels.jsx
@@ -1,10 +1,9 @@
 import * as faceapi from "face-api.js";
 
 const LoadApiModels = async () => {
-  const timestamp = new Date().getTime(); // 캐시 무효화를 위한 타임스탬프 추가
-  await faceapi.nets.tinyFaceDetector.loadFromUri(`/models?${timestamp}`);
-  await faceapi.nets.faceLandmark68Net.loadFromUri(`/models?${timestamp}`);
-  await faceapi.nets.faceExpressionNet.loadFromUri(`/models?${timestamp}`);
+  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+  await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+  await faceapi.nets.faceExpressionNet.loadFromUri('/models');
 };
 
 export default LoadApiModels;
