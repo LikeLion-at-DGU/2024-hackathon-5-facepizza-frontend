28d58001cc6aa33a4adeca92323bffc237ced38c
jjjung0921 <earthenergy-1128@naver.com>
2024-08-06 03:23:40 +0900

    FIX: API 주소 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL: 'http://localhost:5173/',
+    baseURL: 'https://facepizza-cheese.site/',
     headers: {
         'Content-Type': 'application/json',
     },
