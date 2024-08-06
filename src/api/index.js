05962e9b2f2de0143e1e9395faa317a5e2d45330
jjjung0921 <earthenergy-1128@naver.com>
2024-08-06 20:15:31 +0900

    Feat: Logout 추가

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const API = axios.create({
     baseURL: 'https://facepizza-cheese.site',  
     // baseURL: 'http://127.0.0.1:8000',
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
