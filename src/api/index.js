805cc1b5691baf147a1a3134a1694a9e8a611ea8
jiyoon607 <jiyoon607@naver.com>
2024-08-06 19:33:07 +0900

    logout

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const API = axios.create({
     baseURL: 'https://facepizza-cheese.site',  
-    // baseURL: 'http://127.0.0.1:8000',
+    //baseURL: 'http://127.0.0.1:8000',
     headers: {
         'Content-Type': 'application/json',
     },
@@ -10,27 +10,27 @@ export const API = axios.create({
 
 export const apiPost = async (endpoint, body = {}, token = null) => {
     try {
-      const response = await API.post(endpoint, body, {
-        headers: {
-          'Authorization': token ? `Token ${token}` : undefined,
-          'Content-Type': 'application/json',
-        },
-      });
-      return response;
-    } catch (error) {
-      // Handle error
-      if (error.response) {
-        // Server responded with a status other than 2xx
-        console.error('API 요청 실패:', error.response.data);
-        throw new Error(error.response.data.detail || 'API 요청 실패');
-      } else if (error.request) {
-        // Request was made but no response was received
-        console.error('API 요청 실패:', error.request);
-        throw new Error('API 요청 실패: 서버 응답 없음');
-      } else {
-        // Something happened in setting up the request
-        console.error('API 요청 실패:', error.message);
-        throw new Error(error.message || 'API 요청 실패');
-      }
-    }
-  };
+        const response = await API.post(endpoint, body, {
+            headers: {
+            'Authorization': token ? `Token ${token}` : undefined,
+            'Content-Type': 'application/json',
+            },
+        });
+        return response;
+        } catch (error) {
+        // Handle error
+        if (error.response) {
+            // Server responded with a status other than 2xx
+            console.error('API 요청 실패:', error.response.data);
+            throw new Error(error.response.data.detail || 'API 요청 실패');
+        } else if (error.request) {
+            // Request was made but no response was received
+            console.error('API 요청 실패:', error.request);
+            throw new Error('API 요청 실패: 서버 응답 없음');
+        } else {
+            // Something happened in setting up the request
+            console.error('API 요청 실패:', error.message);
+            throw new Error(error.message || 'API 요청 실패');
+        }
+        }
+    };
